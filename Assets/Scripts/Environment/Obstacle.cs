afb0821a54728b6613556d8968334663b81ed43b
Albert Lin <albertlin97@gmail.com>
2023-07-07 14:47:50 -0700

    Added Destructibles

---
+++ Assets/Scripts/Environment/Obstacle.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : EnvironmentObject
+{
+    [SerializeField] private List<DestroyedPiece> destructionAssets;
+
+    
+
+    protected override void CollisionHandler(Collision2D collision)
+    {
+        base.CollisionHandler(collision);
+    }
+    protected override void OnDamaged(float damage)
+    {
+        base.OnDamaged(damage);
+    }
+    protected override void OnRemove()
+    {
+        DestroyedPiece piece;
+        Rigidbody2D rigidBody;
+        foreach(DestroyedPiece piecePrefab in destructionAssets)
+        {
+            //piece.gameObject.SetActive(true);
+            piece = Instantiate(piecePrefab,transform.parent);
+            piece.transform.position = transform.position;
+            rigidBody = piece.GetComponent<Rigidbody2D>();
+            //can multiple force range by collision speed
+            rigidBody.AddForce(new Vector2(
+                Random.Range(DestructionConstants.MIN_X_DESTRUCTION_DEVIATION, DestructionConstants.MAX_X_DESTRUCTION_DEVIATION), 
+                Random.Range(DestructionConstants.MIN_Y_DESTRUCTION_DEVIATION, DestructionConstants.MAX_Y_DESTRUCTION_DEVIATION))
+                );
+            rigidBody.AddTorque(Random.Range(DestructionConstants.MIN_DESTRUCTION_TORQUE, DestructionConstants.MAX_DESTRUCTION_TORQUE));
+        }
+
+        base.OnRemove();
+    }
+}
