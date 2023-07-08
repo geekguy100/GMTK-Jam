5c28155983c1f80f626d192d0fbd0201d9a29b60
Ryan Dougherty <ryandougherty25@gmail.com>
2023-07-08 15:39:17 -0700

    Fixed up UI scaling. Began the alpha merge!

--- Assets/Scripts/GameState/GameManager.cs
+++ Assets/Scripts/GameState/GameManager.cs
@@ -98,4 +98,5 @@ public class GameManager : Singleton<GameManager>
         }
     }
 
+
 }
---
+++ Assets/Scripts/UI/IntroUI.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroUI : MonoBehaviour
+{
+    public void OnIntroStart()
+    {
+        // TODO
+        Debug.Log("Intro Started");
+    }
+
+    public void OnIntroFinished()
+    {
+        // TODO
+        Debug.Log("Intro Finished");
+    }
+
+}
