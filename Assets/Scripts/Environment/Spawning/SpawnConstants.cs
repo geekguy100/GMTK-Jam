7abaf00c6e956417f6b947341858ac461062ef5c
Albert Lin <albertlin97@gmail.com>
2023-07-09 04:03:38 -0700

    update

--- Assets/Scripts/Environment/Spawning/SpawnConstants.cs
+++ Assets/Scripts/Environment/Spawning/SpawnConstants.cs
@@ -17,19 +17,19 @@ public struct SpawnConstants
     public const int INITIAL_STOOL_LIMIT = 1;
     public const int INITIAL_HEAVY_LIMIT = 0;
 
-    public const float BOTTLE_SPAWN_COOLDOWN = .5f;
-    public const float FOOD_SPAWN_COOLDOWN = 10f;
-    public const float STOOL_SPAWN_COOLDOWN = 20f;
+    public const float BOTTLE_SPAWN_COOLDOWN = 1f;
+    public const float FOOD_SPAWN_COOLDOWN = 3f;
+    public const float STOOL_SPAWN_COOLDOWN = 10f;
     public const float HEAVY_SPAWN_COOLDOWN = 60f;
 
     /// <summary>
     /// Angle Range Away from spawn direction vector (in Deg)
     /// </summary>
-    public const float LAUNCH_ANGLE_VARIANCE = 15;
+    public const float LAUNCH_ANGLE_VARIANCE = 10;
     /// <summary>
     /// Speed Variance added on top of base speed
     /// </summary>
-    public const float LAUNCH_SPEED_VARIANCE = 8;
+    public const float LAUNCH_SPEED_VARIANCE = 10;
     /// <summary>
     /// Initial Launch speed
     /// </summary>
@@ -37,11 +37,11 @@ public struct SpawnConstants
     /// <summary>
     /// Random applied torque on spawn
     /// </summary>
-    public const float LAUNCH_BASE_TORQUE = 20;
+    public const float LAUNCH_BASE_TORQUE = 50;
     /// <summary>
     /// Applies how far up the screen is vertically;
     /// </summary>
-    public const float LAUNCH_SPAWN_HEIGHT = .55f;
+    public const float LAUNCH_SPAWN_HEIGHT = .75f;
     /// <summary>
     /// Applies how far the spawn is away from the screen horizontally.
     /// </summary>
